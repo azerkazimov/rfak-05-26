b932bb3e89ec31082a7ddad54318e9f089727ece
Azer Kazimov <azer.kazimov@yahoo.com>
2026-06-04 23:50:21 +0400

    update: use persist for set teaks in localStorage

--- src/hooks/use-task-store.js
+++ src/hooks/use-task-store.js
@@ -1,7 +1,32 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useTodoListStore = create ((set)=>({
-    tasks: [], // default tasks ["asdadasd","dasdadsad","dasdasdas"]
-    addTask: (task)=>set(state=>({tasks: [...state.tasks, task]})),
-    deleteTask: (task)=> set(state=>({tasks: state.tasks.filter(t=>t!==task)})),
-}))
+
+
+export const useTodoListStore = create(
+  persist(
+    (set) => ({
+      tasks: [],
+      addTask: (task) =>
+        set((state) => ({ tasks: [...state.tasks, task] })),
+      deleteTask: (task) =>
+        set((state) => ({ tasks: state.tasks.filter((t) => t !== task) })),
+    }),
+    {
+      name: "todo-tasks",
+    }
+  )
+);
+
+// export const useTodoListStore = create((set) => ({
+//         tasks: [],
+//         addTask: (task) =>
+//           set((state) => {
+//             const newTasks = [...state.tasks, task];
+//             localStorage.setItem("tasks", JSON.stringify(newTasks));
+//             return { tasks: newTasks };
+//           }),
+//         deleteTask: (task) =>
+//           set((state) => ({ tasks: state.tasks.filter((t) => t !== task) })),
+//       }),
+//   );
